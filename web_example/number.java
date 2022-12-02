9bb3b975e555872769112003dd72197127d6f2d5
non-enough <118895839+non-enough@users.noreply.github.com>
2022-12-02 19:35:35 +0900

    Add files via upload

--- web_example/number.java
+++ web_example/number.java
@@ -1,5 +1,31 @@
+package java_tutorial;
+
 public class number {
-    public static void main (Stirng[] args) {
-        system.out.prinln(1+2);
-    }
+
+	public static void numbering() {
+		int i =0;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+	}
+}	
+	public static void main(String[] args) {
+		numbering();
+	}
+		
+//		String[] classGroup = {"최진혁", "최유빈", "한이람","이고잉"};
+//		for(String e : classGroup) {
+//			String member = classGroup[i];
+//			System.out.println(e + "이 상담을 받았습니다.");
+//		}
+//		String[] classGroup = new String[4];
+//		classGroup[0] = "최진혁" ;
+//		System.out.println(classGroup.length);
+//		classGroup[1] = "최유빈" ;
+//		System.out.println(classGroup.length);
+//		classGroup[2] = "한이람" ;
+//		System.out.println(classGroup.length);
+//		classGroup[3] = "이고잉" ;
+//		System.out.println(classGroup.length);
+
 }
